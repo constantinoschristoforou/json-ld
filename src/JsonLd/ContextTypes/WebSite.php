94166252e93dd224787cee46704cac3e44005f71
constantinos <constantinos23@gmail.com>
2016-01-11 02:20:36 +0200

    Add website entity

---
+++ src/JsonLd/ContextTypes/WebSite.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace JsonLd\ContextTypes;
+
+class WebSite extends AbstractContext
+{
+    /**
+     * Property structure
+     *
+     * @var array
+     */
+    protected $structure = [
+        'name' => null,
+        'alternateName' => null,
+        'url' => null,
+    ];
+
+
+}
